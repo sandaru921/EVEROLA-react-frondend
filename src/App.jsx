522f58d895e46e2e7b41b38c9ae38a9950d708c6
Muditha Lakmali <muditha0256@gmail.com>
2025-07-19 22:34:45 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 
-
-
-
-
-
-
 import "./App.css";
 import "./pages/User/HomePage.css"; // Keep CSS import from incoming
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // User-related imports from incoming
-import HomePage from "./pages/User/HomePage";
+
 import LoginPage from "./pages/User/LoginPage";
 import RegisterPage from "./pages/User/RegisterPage";
 import ForgotPasswordPage from "./pages/User/ForgotPasswordPage";
@@ -25,68 +19,63 @@ import Privacy from "./pages/User/Privacy";
 import UserProfile from "./pages/User/UserProfile";
 import "./pages/User/HomePage.css";
 import AdminChat from "./pages/Admin/AdminChat";
-
 import Blog from "./pages/User/Blogs.jsx";
 import BlogDetail from "./pages/User/BlogDetail.jsx";
 import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
 import QuizDash from "./pages/Admin/QuizDash.jsx";
 import AddQuiz from "./pages/Admin/NewQuiz.jsx";
 import TryOutQuiz from "./pages/Admin/TryOutQuiz.jsx";
+import HomePage from "./pages/User/HomePage.jsx";
+import AttemptQuiz from './pages/User/AttemptQuiz.jsx'
 
 
-import ToastWrapper from "./components/ToastWrapper";
-import AuthInterceptor from "@components/AuthInterceptor.jsx";
-import PermissionManager from "./pages/Admin/PermissionManager.jsx";
-import ProtectedRoute from "@components/ProtectedRoute.jsx";
-import AdminSearchBar from "@components/AdminSearchBar.jsx";
-import AttemptQuiz from "./pages/User/AttemptQuiz.jsx";
-import QuizSummary from "./pages/Admin/QuizResultUser.jsx";
+
+
+
 
 function App() {
-    const user = JSON.parse(localStorage.getItem("user"));
+  console.log("App component is rendering"); // Keep debug log from current
 
-    return (
-        <BrowserRouter>
-            <AuthInterceptor /> {/* Interceptor safely inside router context */}
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* User Routes */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/activities" element={<UserActivities />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/invite" element={<Invite />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/joblanding" element={<JobLanding />} /> {/* Normalized to lowercase */}
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/attemptquiz/:id" element={<AttemptQuiz />} />
+        
+        
+        
 
-            {/* Admin Search Bar (only show for logged-in admins) */}
-            {user?.permissions?.includes("admin") && <AdminSearchBar />}
+        {/* Blog Routes */}
+        <Route path="/blogs" element={<Blog />} />
+        <Route path="/blogs/:slug" element={<BlogDetail />} />
 
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/login/forgot-password" element={<ForgotPasswordPage/>}/>
-                <Route path="/register" element={<RegisterPage/>}/>
-                <Route path="/userdashboard" element={<UserDashboard/>}/>
-                  <Route path="/attemptquiz/:id" element={<AttemptQuiz/>}/>
-                  <Route path="/results/:quizResultId" element={<QuizSummary/>}/>
-                <Route path="/activities" element={<UserActivities/>}/>
-                <Route path="/support" element={<Support/>}/>
-                <Route path="/invite" element={<Invite/>}/>
-                <Route path="/chat" element={<Chat/>}/>
-                <Route path="/Joblanding" element={<JobLanding/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-                <Route path="/privacy" element={<Privacy/>}/>
-                <Route path="/profile" element={<UserProfile/>}/>
-                <Route path="/adminchat" element={<AdminChat/>}/>
-                <Route path="/admindashboard" element={<AdminDashboard />} />
-                <Route path="/admin" element={<AdminDashboard />}>
-                    <Route path="/admin/quizzes" element={<QuizDash />} />
-                    <Route path="/admin/addNewQuiz" element={<AddQuiz />} />
-                    <Route path="/admin/tryout/:id" element={<TryOutQuiz />} />
+        {/* Admin Routes */}
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/adminchat" element={<AdminChat />} />
+        <Route path="/admin" element={<AdminDashboard />}>
+          <Route path="/admin/quizzes" element={<QuizDash />} />
+          <Route path="/admin/addNewQuiz" element={<AddQuiz />} />
+          <Route path="/admin/tryout/:id" element={<TryOutQuiz />} />
           {/* Add more nested routes if needed */}
-                </Route>
-                <Route
-                    path="/permission-manager"
-                    element={
-                    <ProtectedRoute allowedRole="Admin">
-                        <PermissionManager/>
-                    </ProtectedRoute>}/>
-            </Routes>
-            <ToastWrapper />
-        </BrowserRouter>
-    );
+        </Route>
 
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
