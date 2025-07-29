bd9ce7b88669dfce3b3f75c8d5605062cf37b3ae
AmayaFernando18 <fdoamaya18@gmail.com>
2025-07-30 00:32:50 +0530

    fixed vite errors

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,36 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   port: 5173,
+//   resolve: {
+//     alias: {
+//       '@components': path.resolve(__dirname, './src/components'),
+//     },
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, './src/components'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      // add other aliases if you use them, e.g.:
+      // '@pages': path.resolve(__dirname, 'src/pages'),
     },
   },
-});
+  build: {
+    sourcemap: false,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      sourcemap: false,
+    },
+  },
+})
