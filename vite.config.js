4f6d8cdc1a1f729a0b4c69d8f86710d46407a484
AmayaFernando18 <fdoamaya18@gmail.com>
2025-07-29 12:06:33 +0530

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: './localhost-key.pem', // Point to the key file you moved
-      cert: './localhost-cert.pem', // Point to the certificate file you moved
-    },
+  // server: {
+  //   https: {
+  //     key: './localhost-key.pem', // Point to the key file you moved
+  //     cert: './localhost-cert.pem', // Point to the certificate file you moved
+  //   },
     port: 5173,
-  },
+  // },
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
